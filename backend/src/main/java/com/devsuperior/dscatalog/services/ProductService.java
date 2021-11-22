b554ab45a0c250384eaaf6c092f0cae13ede2ad0
Luciano Mota <luciano.mota@luminitisolutions.com>
2021-11-22 15:21:10 -0300

    Testes Automatizados

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -67,7 +67,6 @@ public class ProductService {
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		} catch (DataIntegrityViolationException e) {
--- backend/src/test/java/com/devsuperior/dscatalog/DsCatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DsCatalogApplicationTests.java
@@ -9,5 +9,4 @@ class DsCatalogApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
